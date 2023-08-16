35dc9c2b446a219bb48348e4db70a42922e0aee2
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-08-16 07:53:08 -0600

    44. Tarea - Cambiar nombre y edad

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -19,4 +19,12 @@ export class HeroComponent {
     return `${this.name} - ${this.age}`;
   }
 
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
 }
